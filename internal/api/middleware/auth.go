a22e8b11638eeec09e3db1ee9ab66a10faeee325
Mithilesh Gupta <guptamithilesh@protonmail.com>
2025-10-30 17:30:40 +0530

    Getting cookie

--- internal/api/middleware/auth.go
+++ internal/api/middleware/auth.go
@@ -2,7 +2,10 @@ package middleware
 
 import (
 	"context"
+	"log"
 	"net/http"
+
+	"github.com/mithileshgupta12/velaris/internal/helper"
 )
 
 type ctxUserKey string
@@ -16,13 +19,20 @@ type CtxUser struct {
 }
 
 func AuthMiddleware(next http.Handler) http.Handler {
-	ctxUser := CtxUser{
-		ID:    1,
-		Name:  "Bob",
-		Email: "bob@aol.com",
-	}
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sessionCookie, err := r.Cookie("auth_session")
+		if err != nil {
+			helper.ErrorJsonResponse(w, http.StatusUnauthorized, "Unauthorized")
+			return
+		}
+
+		log.Println(sessionCookie)
+
+		ctxUser := CtxUser{
+			ID:    1,
+			Name:  "Bob",
+			Email: "bob@aol.com",
+		}
 		ctx := context.WithValue(r.Context(), CtxUserKey, ctxUser)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
