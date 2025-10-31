989cd4a54051c24cdd44b9ff2cd7c1f7916f6907
Mithilesh Gupta <guptamithilesh@protonmail.com>
2025-10-31 21:14:43 +0530

    Removed pre commit

---
+++ frontend/src/router/LoginView.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Login</h1>
+  <input type="text" name="email" id="email" v-model="form.email" />
+  <input type="password" name="password" id="password" v-model="form.password" />
+  <input type="submit" value="Login" @click="submit" />
+
+  <pre>{{
+    JSON.stringify(
+      {
+        email: form.email,
+        password: form.password,
+      },
+      null,
+      4,
+    )
+  }}</pre>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const form = reactive({
+  email: '',
+  password: '',
+})
+
+const submit = () => {
+  console.log('Logged in')
+}
+</script>
