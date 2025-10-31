c37c88e2fcac4fbe5f7d49eace2ee701f089f07b
Mithilesh Gupta <guptamithilesh@protonmail.com>
2025-10-31 21:44:41 +0530

    Removed logging

--- frontend/src/router/LoginView.vue
+++ frontend/src/router/LoginView.vue
@@ -3,17 +3,6 @@
   <input type="text" name="email" id="email" v-model="form.email" />
   <input type="password" name="password" id="password" v-model="form.password" />
   <input type="submit" value="Login" @click="submit" />
-
-  <pre>{{
-    JSON.stringify(
-      {
-        email: form.email,
-        password: form.password,
-      },
-      null,
-      4,
-    )
-  }}</pre>
 </template>
 
 <script setup lang="ts">
