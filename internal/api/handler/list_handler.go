a1ceed13cada3b6218ab93610f51a3350f6fc6b1
Mithilesh Gupta <guptamithilesh@protonmail.com>
2025-11-11 16:42:20 +0530

    Added method to get lists

--- internal/api/handler/list_handler.go
+++ internal/api/handler/list_handler.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"log/slog"
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/mithileshgupta12/velaris/internal/db/repository"
+	"github.com/mithileshgupta12/velaris/internal/helper"
 )
 
 type ListHandler struct {
@@ -15,7 +19,24 @@ func NewListHandler(listRepository repository.ListRepository) *ListHandler {
 }
 
 func (lh *ListHandler) Index(w http.ResponseWriter, r *http.Request) {
-	//
+	boardIdParam := chi.URLParam(r, "boardId")
+
+	boardId, err := strconv.Atoi(boardIdParam)
+	if err != nil {
+		helper.ErrorJsonResponse(w, http.StatusBadRequest, "invalid board id")
+		return
+	}
+
+	lists, err := lh.listRepository.GetAllListsByBoardId(&repository.GetAllListsByBoardIdArgs{
+		BoardId: int64(boardId),
+	})
+	if err != nil {
+		slog.Error("failed to get lists for board", "err", err)
+		helper.ErrorJsonResponse(w, http.StatusInternalServerError, "internal server error")
+		return
+	}
+
+	helper.JsonResponse(w, http.StatusOK, lists)
 }
 
 func (lh *ListHandler) Store(w http.ResponseWriter, r *http.Request) {
